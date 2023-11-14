4a24fcfe362044f42f3d26dbfd48dc7d77aa1b82
server tombak <69975139+ServerToMbak@users.noreply.github.com>
2023-11-15 01:05:09 +0300

    How to use Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Dictionary<int, string> rookieOfTheYear  = new Dictionary<int, string>();
+
+rookieOfTheYear.Add(1, "Mike Miller");
+rookieOfTheYear.Add(2, "Jane Doe");
+rookieOfTheYear.Add(3, "Jane Doe");
+rookieOfTheYear.Add(4, "John Smith");
+
+
+if(rookieOfTheYear.ContainsKey(1))
+{
+    Console.WriteLine(rookieOfTheYear[1]);
+}
+Console.WriteLine(rookieOfTheYear.FirstOrDefault(O=>O.Key == 1)) ;
+
+//var a = rookieOfTheYear.FirstOrDefault(O => O.Key == 1);
+//Console.WriteLine(rookieOfTheYear[a.Value]);
+
+
+Dictionary<string, List<string>> wishList = new();
+
+wishList.Add("Server Tombak", new List<string> { "Xbox", "Tesla","Kebappp" });
+wishList.Add("Someone Who", new List<string> { "PS5", "bmw", "Pizza" });
+
+
+
+foreach (var (Key,value) in wishList)
+{
+    Console.WriteLine($"{Key}'s wishlist:");
+    foreach (var item in value)
+    {
+        Console.WriteLine($"\t{item}");
+    }
+}
+
+
+Console.WriteLine(wishList["Server Tombak"][0]);
+
+Console.WriteLine(wishList["Server Tombak"].Count);
+wishList.Remove("Server Tombak");
